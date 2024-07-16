88a864237aa0cdf6d1b04148ec687de5f8e7c897
yllinjoooZ <yllzeneli920@gmail.com>
2024-07-16 03:00:25 -0700

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,8 @@
+produktet = {
+    "tv": 300,
+    "mouse": 5,
+    "laptop": 1000,
+    "keyboard": 50
+}   
+print(sorted(produktet.values()))
+print(f"Produkti me cmimin me te vogel eshte", min(produktet.values()))
